6105f37fbd29fbea8ef59aeb29f3580642741259
keeptrynjoy <sungmin4218@gmail.com>
2023-02-07 20:58:26 +0900

    [Refactor] setting

--- src/test/java/com/example/loganalysis/LogAnalysisApplicationTests.java
+++ src/test/java/com/example/loganalysis/LogAnalysisApplicationTests.java
@@ -8,13 +8,4 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class LogAnalysisApplicationTests {
 
-    @Autowired
-    private LogAnalysisUtils logAnalysisUtils;
-
-    @Test
-    void leadLogFile() {
-
-        logAnalysisUtils.readLogFile();
-
-    }
 }
