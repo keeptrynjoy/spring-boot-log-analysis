d23bef73e4bb6bb8f792ccf9ce58298bd3c2d27d
keeptrynjoy <sungmin4218@gmail.com>
2023-02-16 09:52:06 +0900

    [Refactor] 중복 코드를 메서드로 분리

--- src/main/java/com/example/loganalysis/service/LogExtractMethod.java
+++ src/main/java/com/example/loganalysis/service/LogExtractMethod.java
@@ -38,9 +38,7 @@ public class LogExtractMethod{
     /* 상위 3개 API Service ID 및 요청 수 추출 메서드 */
     public static String topThreeServiceIdCountExtractor(List<InputLogDto> inputLogDtoList) {
 
-        StringBuilder sb = new StringBuilder("상위 3개의 API Service ID와 각각의 요청 수").append(NEW_LINE);
-
-        List<Map.Entry<String, Long>> topThreeServiceIdCounts = statusCodeFilter(inputLogDtoList) // 상태 200의 로그만 추출
+        List<Map.Entry<String, ?>> result = statusCodeFilter(inputLogDtoList) // 상태 200의 로그만 추출
                         .map(ild -> new ExtractLogDto(ild.getUrl())) // 로그의 URL에서 Service ID 추출
                         .collect(groupingBy(ExtractLogDto::getServiceId, counting())) // Service ID 별 개수 추출
                         .entrySet().stream() // ServiceID와 요청 수가 key,value 형태로된 Map을 반환
@@ -48,44 +46,42 @@ public class LogExtractMethod{
                         .limit(3) // 정렬된 사항에서 앞의 3개 Service ID만 추출
                         .collect(toList());
 
-        for (Map.Entry<String, Long> map : topThreeServiceIdCounts) {
-            sb.append(map.getKey())
-                    .append(" : ")
-                    .append(map.getValue())
-                    .append(NEW_LINE);
-        }
-
-        return  sb.toString();
+        return convertToStrFromList(result,"상위 3개의 API Service ID와 각각의 요청 수");
     }
 
     /* 웹브라우저별 사용 비율 추출 메서드 */
     public static String usageRateByWebBrowser(List<InputLogDto> inputLogDtoList) {
 
-        StringBuilder sb = new StringBuilder("웹브라우저별 사용 비율").append(NEW_LINE);
-
-        List<Map.Entry<String, Double>> collect = statusCodeFilter(inputLogDtoList) // 상태 200의 로그만 추출
+        List<Map.Entry<String, ?>> result = statusCodeFilter(inputLogDtoList) // 상태 200의 로그만 추출
                 .collect(teeing( // 두 번의 계산 결과를 한번에 처리하기 위해 Java 12 Collectors.teeing()을 사용
                         counting(), // 로그 전체 개수 추출
                         groupingBy(InputLogDto::getWebBrowser, counting()), // Web Browser별 개수 추출
                         (totalCount, groupResult) -> {
-                            Map<String, Double> result = new HashMap<>(); // 계산된 값을 담을 Map 생성
+                            Map<String, String> resultMap = new HashMap<>(); // 계산된 값을 담을 Map 생성
                             groupResult.forEach( // Web Browser와 요청 수가 key,value 형태로된 Map을 반복문으로 추출
                                     // Web Browser와 계산된 value를 result에 담아서 반환
-                                    (k, v) -> result.put(k, (double) (v * 100) / totalCount));
-                            return result;
+                                    (k, v) -> resultMap.put(k,
+                                            strFormat((double) (v * 100) / totalCount)));
+                            return resultMap;
                         })
                 ).entrySet().stream()
                 .sorted(Map.Entry.comparingByValue(reverseOrder())) // 요청 수가 높은 순으로 list에 담기 위해 역정렬
                 .collect(toList());
 
-        for (Map.Entry<String, Double> map : collect) {
+        return convertToStrFromList(result,"웹브라우저별 사용 비율");
+    }
+
+    /* 분석한 결과의 List와 문자열로 변환하여 항목명과 결합하는 메서드 */
+    private static String convertToStrFromList(List<Map.Entry<String,?>> resultList, String title){
+       StringBuilder sb = new StringBuilder(title).append(NEW_LINE);
+
+        for (Map.Entry<String, ?> map : resultList) {
             sb.append(map.getKey())
                     .append(" : ")
-                    .append(strFormat(map.getValue()))
+                    .append(map.getValue())
                     .append(NEW_LINE);
         }
-
-        return  sb.toString();
+        return sb.toString();
     }
 
     /* status(상태)가 200인 값만 추출하기 위한 메서드 */
@@ -94,7 +90,7 @@ public class LogExtractMethod{
     }
 
     /* Web Browser 추출 과정에서 계산된 요청 수를 출력하기 위한 변환 메서드  */
-    private static String strFormat(double value){
+    private static String strFormat(Object value){
         return String.format("%.1f",value) + "%";
     }
     
